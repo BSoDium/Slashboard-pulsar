fc6bedbe920bd9521c37e3fe4a185dbb9729bcf5
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-09-04 16:10:26 +0200

    build(pulsar): create update.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-echo Thank you for installing Pulsar-slashboard
-echo this script will daemonize the node js app and run it.
+echo "Thank you for installing Pulsar-slashboard"
+echo "this script will daemonize the node js app and run it."
 
 # install pm2
-echo Setting up pm2...
-npm install pm2 -g || echo there seems to be a problem with your npm installation, the script failed to install pm2
+echo "Setting up pm2..."
+npm install pm2 -g || "echo there seems to be a problem with your node installation, the script failed to install pm2"
 
-echo Your server will be given a pairing key,
-echo you enter a key of your choice or let the script generate it randomly.
-echo Do you want to auto-generate it [Y/n] ?
+echo "Your server will be given a pairing key,"
+echo "you can enter a key of your choice or let the script generate it randomly."
+echo "Do you want to auto-generate it [Y/n] ?"
 read choice
 if [ $choice == "Y" -o $choice == "y" ]
 then
-    echo Generating random 64-char alphanumeric key.
+    echo "Generating random 64-char alphanumeric key."
     key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
 else
-    echo Please provide the key of your choice : 
+    echo "Please provide the key of your choice : "
     read key
 fi
 printf $key > ../key.txt
-echo Successfully written to file.
+echo "Successfully written to file."
 echo "+------------------------------------------------------------------------------ +"
 echo "| Your key is : $key"
 echo "| It can be later found in the key.txt file located in the root of the project."
