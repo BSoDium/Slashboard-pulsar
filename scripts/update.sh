ac41327197956724223c7b0143cd568a6c871316
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-09-07 18:20:43 +0200

    build(pulsar): fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,15 +8,15 @@ pm2 stop Pulsar
 pm2 delete Pulsar
 
 echo "Backing up the key file..."
-cp ./key.txt ../key.txt.bak
+cp ../key.txt ../../key.txt.bak
 
 echo "Pulling from git..."
 git reset --hard
 git pull
 
 echo "Restoring the key file..."
-cp ../key.txt.bak ./key.txt
+cp ../../key.txt.bak ../key.txt
 
 echo "Starting Pulsar..."
-pm2 start ecosystem.config.js --env production
+pm2 start ../ecosystem.config.js --env production
 echo "Done!"
