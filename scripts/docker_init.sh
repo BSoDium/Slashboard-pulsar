9bf50547363c0bf776f066c9a1b33fd7870356c4
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-09-18 02:11:27 +0200

    build: Add key system to docker_init.sh

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-# Write env var for auth key to key.txt if it is defined
-if [ -v PULSAR_AUTH_KEY ]; then echo -n $PULSAR_AUTH_KEY > key.txt; fi
+key=$(cat key.txt)
 
+# if this is the first time running the container, run the setup script
+if [ $key == "default" ]; then
+  echo "First time running this docker container."
+  echo "Generating random 64-char alphanumeric key..."
+  key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
+  echo -n $key > key.txt
+
+  echo "The key was successfully saved."
+  echo "+--------------------------------------------------------------------------------+"
+  echo "| Your key is : $key |"
+  echo "+--------------------------------------------------------------------------------+"
+else
+  echo "Using existing key."
+fi
+
+# run the app
 npm run start
