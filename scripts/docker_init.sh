8b744fbcaedcaf16627b3ebe901c69d8282e9803
Brenek Harrison <brenekharrison@gmail.com>
2021-09-17 18:54:06 -0600

    Implement genkey arg

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Output a new key and exit if genkey is the first argument to the script.
+if [ $1 = "genkey" ]; then
+	echo -n "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
+	exit
+fi
+
 # Write env var for auth key to key.txt if it is defined
 if [ -v PULSAR_AUTH_KEY ]; then echo -n $PULSAR_AUTH_KEY > key.txt; fi
 
+# Start the program
 npm run start
