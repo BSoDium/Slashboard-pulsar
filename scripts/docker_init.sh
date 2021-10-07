6d461704f4f13e604dc5084674bf72958877bb61
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-10-07 23:15:52 +0200

    feat(pulsar): setup required environment variables and volumes for docker

--- scripts/docker_init.sh
+++ scripts/docker_init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo
+
 # Each time an image is built and a container is created, new public and secret 
 # keys are generated.
 # The user needs to check the container's logs to retrieve the public key.
@@ -9,30 +11,28 @@ import sys, json;
 print(json.load(sys.stdin)['security']['serverKey'])
 ")
 
+# check if the user has setup the correct bind mount for the .ssh directory
+if [ ! -e /host/proc/mounts ]; then
+	echo "Error : Please mount volume /proc/mounts at /host/proc/mounts"
+	exit 1
+fi
 
 if [ $serverKey == "default" ]; then
+
 	echo "First time starting up this container."
-	echo "Generating random 64-char alphanumeric keys..."
+	echo "Generating new configuration..."
+	
+	echo "- Generating new server key."
 	serverKey=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
-	sharedSecret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
-
-	# write key to configuration file
-	echo "{\"security\": {\"serverKey\": \"$serverKey\", \"sharedSecret\": \"$sharedSecret\", \"jwtLifetime\": 3600}}" > config/default.json
-
-	echo "The key was successfully saved."
-  echo "+--------------------------------------------------------------------------------+"
-  echo "| Your key is : $sharedSecret |"
-  echo "+--------------------------------------------------------------------------------+"
-
-	echo "Running ssh-keygen..."
-	ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-	echo "Done."
-	echo "This is your public key :"
-	cat ~/.ssh/id_rsa.pub
-	echo "Please add it to ~/.ssh/authorized_keys on your server, it will allow the container"
-	echo "to retrieve the necessary data in real time."
 	
-	
+	# check if the environment variable SHAREDSECRET is defined, if not, generate a new key
+	if [ -z "$SHAREDSECRET" ]; then
+		echo "- Generating new shared secret."
+		sharedSecret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+	else
+		echo "- Using the shared secret provided by the environment variable SHAREDSECRET."
+		sharedSecret=$SHAREDSECRET
+	fi
 else
 	echo "Using existing configuration."
 fi
